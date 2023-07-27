413dc5e99b9273fa23381f1360e08405d85c40e8
TEU Delicia <matheusaugustodacostacunha@gmail.com>
2023-07-27 18:39:17 -0300

    meu preimeiro codigo

---
+++ PrimeiroCode/PrimeiroCode/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PrimeiroCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
