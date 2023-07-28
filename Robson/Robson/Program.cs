3743ffd3d631c0d1d9aba4b59c316e841d90b84a
Matheus Augusto da Costa Cunha <140459392+Math9uz@users.noreply.github.com>
2023-07-28 19:52:07 -0300

    Add files via upload

---
+++ Robson/Robson/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+
+namespace Robson
+{
+    class Progam
+    {
+        static void Main()
+        {
+            List<string>bomdia=new List<string>();
+            List<string>cores=new List<string>();
+            List<string>cores2=new List<string>();
+
+            bomdia.Add("Bom dia!");
+
+            cores.Add("Vermelho");
+            cores.Add("Verde");
+            cores.Add("Azul");
+
+            cores2.Add("Amarelo Laranja Cinza");
+
+            foreach(string b in bomdia)
+            {
+                Console.WriteLine(b);
+            }
+
+            foreach(string c in cores)
+            {
+                Console.WriteLine(c);
+            }
+            foreach(string a in cores2)
+            {
+                Console.WriteLine(a);
+            }
+
+        }
+    }
+}
