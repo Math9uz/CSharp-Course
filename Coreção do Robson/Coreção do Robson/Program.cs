82b9c388c420ce09d997a5f94f2a0ef587029a52
Matheus Augusto da Costa Cunha <140459392+Math9uz@users.noreply.github.com>
2023-07-28 20:41:48 -0300

    Add files via upload

---
+++ Coreção do Robson/Coreção do Robson/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+
+namespace Coreção_do_Robson
+{
+    class Program
+    {
+        static void Main()
+        {
+            string frase, x, y, z;
+            string[] array;
+
+            frase = Console.ReadLine();
+            x = Console.ReadLine();
+            y = Console.ReadLine();
+            z = Console.ReadLine();
+
+            array = Console.ReadLine().Split(' ');
+
+            Console.WriteLine(" -----------------------");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);   
+            Console.WriteLine(y);   
+            Console.WriteLine(z);
+
+            Console.WriteLine($"{array[0]} {array[1]} {array[2]}");
+        }
+    }
+}
